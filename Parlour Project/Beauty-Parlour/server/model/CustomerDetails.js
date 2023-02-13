b2a2c167e0101e46a5476d03db43da58fb862503
SabareeshAH <sabareeshhari2003@gmail.com>
2023-02-13 10:23:23 +0530

    commit

---
+++ Parlour Project/Beauty-Parlour/server/model/CustomerDetails.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+const Courses = require('./Courses');
+const Schema=mongoose.Schema;
+
+const CustomerDetailsSchema = new Schema({
+
+    name : {
+        type : String,
+        required : [true, "Name is required to create a Account"],
+    },
+    mobile_no : {
+        type: Number,
+        required: [true, "Phone Number is required for creating an account"],
+        min: [1000000000, "Enter a Valid Phone number"]
+    },
+
+    login_id : {
+        type: mongoose.SchemaTypes.ObjectId,
+    },
+    place : {
+        city : {
+            type : String,
+            required : [true, "City name is required"]
+        },
+        state : {
+            type : String,
+            required : [true, "State name is required"]
+        }
+    },
+    Booked_Items:{
+        Booked_Courses/*other names given in Taskade Events and Services Id*/: [{
+            type: Number,
+        }],
+        Timing:{
+            type:Number,
+        }
+    } 
+})
+
+module.exports = mongoose.model('CustomerDetails', CustomerDetailsSchema)
